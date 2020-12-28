e2ae406eea5d30e7391d7dd10e112fbdb6e77692
abutaher <mdabutahersaikat@gmail.com>
2020-12-28 21:41:46 +0600

    full api done.ya hooooo

--- models/Review.js
+++ models/Review.js
@@ -38,4 +38,39 @@ ReviewSchema.index({bootcamp : 1, user : 1},{
     unique : true
 })
 
+// Static method to get avg of rating and save
+ReviewSchema.statics.getAverageRating = async function(bootcampId){
+    // console.log('Calculating avg cost...'.blue);
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp : bootcampId }
+        },
+        {
+            $group : {
+                _id : '$bootcamp',
+                averageRating : { $avg: "$rating"}
+            }
+        }
+    ]);
+    
+    try{
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating : obj[0].averageRating
+        })
+    }catch(err){
+        console.log(err);
+    }
+
+}
+
+// Call getAvarageCost after save
+ReviewSchema.post('save', function(){
+    this.constructor.getAverageRating(this.bootcamp);
+})
+
+// Call getAvarageCost after save
+ReviewSchema.pre('remove', function(){
+    this.constructor.getAverageRating(this.bootcamp);
+})
+
 module.exports = mongoose.model('Review', ReviewSchema);
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,20 +2,35 @@ const express = require("express");
 const Review = require('../models/Review');
 
 const {
-    getReviews , getReview , addReview
+    getReviews,
+    getReview,
+    addReview,
+    updateReview,
+    deleteReview
 } = require("../controllers/reviews");
 
-const router = express.Router({ mergeParams : true});
+const router = express.Router({
+    mergeParams: true
+});
 
 const advancedResults = require('../middleware/advancedResults');
-const { protect , authorize } = require('../middleware/auth');
+const {
+    protect,
+    authorize
+} = require('../middleware/auth');
 
 
 
 router.route('/')
-    .get(advancedResults(Review,{ path : 'bootcamp', select : 'name description'}), getReviews).post( protect, authorize('user', 'admin') ,addReview)
+    .get(advancedResults(Review, {
+        path: 'bootcamp',
+        select: 'name description'
+    }), getReviews).post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id').get(getReview)
+
+router.route('/:id')
+    .get(getReview)
+    .put(protect, authorize('user', 'admin'), updateReview).delete(protect, authorize('user', 'admin'), deleteReview)
 
 
 module.exports = router;
